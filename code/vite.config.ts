732ce3335d4fa60a3d211c5e6aca41b3308355f2
mangpo <mangpo4144@gmail.com>
2026-03-13 18:28:26 +0700

    เพิ่ม sever

--- code/vite.config.ts
+++ code/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+
+  const envVersion = dotenv.config({
+    path: `../.env.version`,
+  }).parsed;
+
+  return {
+    plugins: [react(), svgr()],
+    base: "/",
+    define: {
+      "import.meta.env": {
+        ...envVersion,
+      },
+    },
+  };
+});
